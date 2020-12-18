b707fb1a2b0b76e33282a4ea4310009f740078ed
Luke Kellett <73270647+NitroTears@users.noreply.github.com>
2020-12-18 19:01:09 +1000

    Update Controller.java

--- src/dsstorebegone/Controller.java
+++ src/dsstorebegone/Controller.java
@@ -22,7 +22,7 @@ public class Controller {
 
     public Controller() throws IOException {
     }
-
+    //controller initialisation.
     public void initialize() {
         deletedFilesList.clear();
         startButton.setOnAction((event) -> {
@@ -42,7 +42,7 @@ public class Controller {
         });
 
     }
-    //Manages the deletion of files and folders
+    //the main process of file and folder deletion, run when the execute button is pressed.
     public void startDelete(String driveLetter) throws IOException {
         op.initialise(driveLetter);
         File drive = op.getDrive();
@@ -59,8 +59,8 @@ public class Controller {
             op.setFilesListString(op.getFilesListString() + ".Spotlight-V100 Folder Deleted. \r \n");
             deletedFilesList.setText(op.getFilesListString());
         }
-        //File Deletion Segment
-        deleteFiles(drive);
+        //Files Deletion Segment
+        op.deleteFiles(drive);
 
         //Finishing Segment
         if (!op.Finish()) {
@@ -69,19 +69,5 @@ public class Controller {
             op.setFilesListString(op.getFilesListString() + "Operation successful! " + op.getNumOfFiles() + " file(s) deleted.\r \nA list of deleted files can be found in deletedfiles.txt. \r \n");
         }
         deletedFilesList.setText(op.getFilesListString());
-    }
-
-    public void deleteFiles(File drive) throws IOException {
-        File[] files = drive.listFiles();
-        for (File file : files) {
-            if (file.isDirectory() && !file.getName().startsWith("$")) {
-                deleteFiles(file);
-            } else if (file.getName().startsWith("._") || file.getName().startsWith(".DS_Store")) {
-                file.delete();
-                op.setNumOfFiles(op.getNumOfFiles() + 1);
-                op.writer.write(file.toString() + "\r \n");
-                op.setFilesListString(op.getFilesListString() + file.toString() + "\r \n");
-            }
-        }
     }
 }
